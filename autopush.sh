62714cefdde81b38c571752849dddb2b8f579ca0
”Jesper <jesperlovkvist@mac.lan>
2026-05-01 20:56:54 +0200

    Auto-commit 2026-05-01 20:56

--- autopush.sh
+++ autopush.sh
@@ -3,6 +3,8 @@ cd ~/rison-bevakning
 echo "Bevakar ändringar i ~/rison-bevakning..."
 fswatch -o -r . --exclude='.git' --exclude='__pycache__' --exclude='.DS_Store' . | while read; do
     sleep 1
+    md5 -q bevakning_serper.py > .checksum
     git add -A
-    git diff --staged --quiet || git commit -m "Auto-commit $(date '+%Y-%m-%d %H:%M') [md5:$(md5 -q bevakning_serper.py)]" && git push && echo "Pushad $(date '+%H:%M')"
+    CHECKSUM=$(cat .checksum)
+    git diff --staged --quiet || git commit -m "Auto-commit $(date '+%Y-%m-%d %H:%M') [md5:${CHECKSUM}]" && git push && echo "Pushad $(date '+%H:%M')"
 done
